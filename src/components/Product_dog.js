2f7020c6aa200b2875741098909d5aaf47b10998
morphine <82575774+mofinkc51@users.noreply.github.com>
2021-11-17 19:48:36 +0700

    product_test

---
+++ src/components/Product_dog.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FiGithub } from "react-icons/fi"
+import './Header.css'
+
+function Dog() {
+    return (
+        <div className="header">
+            <div className = "container">
+                <div className = "header-con">
+                    <div className = "logo-container">
+                        <a href="/">MeowPhine <FiGithub /> - Dog </a>
+                    </div>
+                        <div className = "menu-logout">
+                        <a className="logout">User 1</a></div>
+                        </div>
+                            
+                        </div>
+                </div>
+    
+    )
+}
+
+
+export default Dog
